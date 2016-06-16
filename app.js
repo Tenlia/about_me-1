01af800a758222e345bcf3afcc0c1437656c907e
Elizabeth Kleinschmidt <tenlia@hotmail.com>
2016-06-16 13:34:57 -0700

    Liz had monkeyed with this code, now

--- app.js
+++ app.js
@@ -3,45 +3,67 @@ var tally = 0;
 var userName = prompt('Greetings! What is your name?');
 alert('Welcome' + ' ' + userName + ', glad you\'re here');
 
+//Liz has altered this question to be a function
 var answer1 = prompt ('Has Alison been to Siberia?').toLowerCase();
-if(answer1 === 'yes' || answer1 === 'y') {
-  alert('Correct! Alison spent a winter in Siberia! Brr!!!');
-  tally++;
-}
-else {
-  alert('Incorrect, Alison spent several wintery months almost freezing to death in Siberia.');
+function question1(){
+  if(answer1 === 'yes' || answer1 === 'y') {
+    alert('Correct! Alison spent a winter in Siberia! Brr!!!');
+    tally++;
+  }
+  else {
+    alert('Incorrect, Alison spent several wintery months almost freezing to death in Siberia.');
+  }
 };
+question1(answer1);
 
+//Liz has altered this question to be a function
 var answer2 = prompt('Has Alison ever been to the Amazon?').toLowerCase();
-if(answer2 === 'yes' || answer2 === 'y'){
-  alert('Yup! Alison spent a summer in the Amazon in Peru living and working with the Iquito tribe.');
-  tally++;
-}
-else{
-  alert('Nope! Alison spent 3 months in the Peruvian Amazon working with Iquito elders on documenting their language');
+function question2(){
+  if(answer2 === 'yes' || answer2 === 'y'){
+    alert('Yup! Alison spent a summer in the Amazon in Peru living and working with the Iquito tribe.');
+    tally++;
+  }
+  else{
+    alert('Nope! Alison spent 3 months in the Peruvian Amazon working with Iquito elders on documenting their language');
+  }
 };
+question2(answer2);
 
+//Liz has altered this question to be a function
 var answer3 = prompt('Has Alison ever been to the Serengeti?').toLowerCase();
-if (answer3 === 'no' || answer3 === 'n'){
-  alert('You are correct! Alison has never been to Africa, but would love to!');
-  tally++;
-}
-else{
-  alert('No way! Alison has never been to Africa, but is dying to go!');
+function question3(){
+  if (answer3 === 'no' || answer3 === 'n'){
+    alert('You are correct! Alison has never been to Africa, but would love to!');
+    tally++;
+  }
+  else{
+    alert('No way! Alison has never been to Africa, but is dying to go!');
+  }
 };
+question3(answer3);
 
+//Liz has alter this question to be a function
 var answer4 = prompt('Has Alison ever harvested cranberries?').toLowerCase();
-if (answer4 === 'yes' || answer4 === 'y'){
-  alert('You are correct! Alison managed an organic cranberry farm for 5 years!');
-  tally++;
-}
-else{
-  alert('Incorrect! Alison managed an organic cranberry farm in Cape Cod for 5 years and can no longer stand the taste of cranberry juice!');}
+function question4(){
+  if (answer4 === 'yes' || answer4 === 'y'){
+    alert('You are correct! Alison managed an organic cranberry farm for 5 years!');
+    tally++;
+  }
+  else{
+    alert('Incorrect! Alison managed an organic cranberry farm in Cape Cod for 5 years and can no longer stand the taste of cranberry juice!');}
+};
+question4(answer4);
 
-var answer5 = prompt('Does Alison own a bicycle?').toLowerCase();if(answer5 === 'yes' || answer5 === 'y'){
-  alert ('Incorrect -kind of! Trick question! She actually owns 2');}else{alert ('Correct She owns two bicycles that she loves equally.');
-  tally++;
-}
+//Liz has changed this question to be a function
+var answer5 = prompt('Does Alison own a bicycle?').toLowerCase();
+function question5(){
+  if(answer5 === 'yes' || answer5 === 'y'){
+    alert ('Incorrect -kind of! Trick question! She actually owns 2');
+  }else{alert ('Correct She owns two bicycles that she loves equally.');
+    tally++;
+  }
+};
+question5(answer5);
 
 var counterNumber = 0;
 while(counterNumber < 5) {
